738f7a1e99b3600cbb68cddc379f4bed1d148fe4
Bunyamin Eren <bunyamin.eren@live.be>
2016-10-14 10:58:46 +0200

    added empty classes to force Git to generate the folder structure

---
+++ Java/src/main/java/be/regendans/spring/entities/User.java
@@ -0,0 +1,7 @@
+package be.regendans.spring.entities;
+
+/**
+ * Created by Bunyamin on 14/10/2016.
+ */
+public class User {
+}
---
+++ Java/src/test/java/be/regendans/spring/dao/TestUserDao.java
@@ -0,0 +1,7 @@
+package be.regendans.spring.dao;
+
+/**
+ * Created by Bunyamin on 14/10/2016.
+ */
+public class TestUserDao {
+}
