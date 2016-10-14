0c99ec6f0c4de3e1ebd58989528c74533bd75788
JorenLuykx <joren.luykx@student.pxl.be>
2016-10-14 11:01:03 +0200

    stop workspace

---
+++ Java/src/main/java/be/regendans/spring/restcontrollers/TemperatureRestController.java
@@ -0,0 +1,9 @@
+package be.regendans.spring.restcontrollers;
+
+import org.springframework.web.bind.annotation.*;
+/**
+ * Created by Joren on 14/10/2016.
+ */
+@RestController
+public class TemperatureRestController {
+}
