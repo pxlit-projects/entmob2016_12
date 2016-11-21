73fca1f1688dc39872ab79de49a08203a6d53fe7
Bunyamin Eren <bunyamin.eren@live.be>
2016-11-21 12:41:17 +0100

    hotfix

--- Java/src/main/java/be/pxl/regendans/jms/JMSMessageLogger.java
+++ Java/src/main/java/be/pxl/regendans/jms/JMSMessageLogger.java
@@ -1,16 +1,12 @@
 package be.pxl.regendans.jms;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Queue;
 
-/**
- * Created by Frank on 3-11-2016.
- */
 @Component
 public class JMSMessageLogger {
 
