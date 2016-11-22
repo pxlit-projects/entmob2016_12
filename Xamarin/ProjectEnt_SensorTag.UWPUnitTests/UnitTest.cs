ae1b03644d0a4ca59c89f9a0f0bc22eaa0598d8d
Francesco Ventura <Francesco.Ventura34@gmail.com>
2016-11-22 23:13:51 +0100

    test

--- Xamarin/ProjectEnt_SensorTag.UWPUnitTests/UnitTest.cs
+++ Xamarin/ProjectEnt_SensorTag.UWPUnitTests/UnitTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using ProjectEnt_SensorTag.UWP.ViewModels;
 
 namespace ProjectEnt_SensorTag.UWPUnitTests
 {
@@ -7,8 +8,70 @@ namespace ProjectEnt_SensorTag.UWPUnitTests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestNavigationLogin()
         {
+            var test = new TestNotificationService();
+            var login = new LoginViewModel(test);
+
+            login.Login.Execute(null);
+
+            Assert.AreEqual(test.CurrentPageKey, "ProjectEnt_SensorTag.UWP.Views.MenuView");
+        }
+        [TestMethod]
+        public void TestListView()
+        {
+            var test = new TestNotificationService();
+            var login = new MainViewModel(test);
+
+            login.Temp.CanExecute(null);
+            var temp = login.TemperatureList;
+            Assert.IsTrue(temp.Count > 0);
+        }
+    }
+
+    public class TestNotificationService : UWP.Navigation.INavigationService
+    {
+
+        private string currentPageKey;
+
+        public string CurrentPageKey
+        {
+            get { return currentPageKey; }
+            set { currentPageKey = value; }
+        }
+
+        private bool wentBack = false;
+
+        public bool WentBack
+        {
+            get { return wentBack; }
+            set { wentBack = value; }
+        }
+
+
+        public void GoBack()
+        {
+            wentBack = true;
+        }
+
+        public void NavigateTo(string pageKey)
+        {
+            this.currentPageKey = pageKey;
+        }
+
+        public void NavigateTo(string pageKey, object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Navigate(Type sourcePage)
+        {
+            this.currentPageKey = sourcePage.FullName;
+        }
+
+        public void Navigate(Type sourcePage, object parameter)
+        {
+            this.currentPageKey = sourcePage.FullName;
         }
     }
 }
